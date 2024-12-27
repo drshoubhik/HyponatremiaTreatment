4557b72c05e02290581a6ec9d5ae5509bb8f4e6b
Dr. Shoubhik Banerjee <shoubhik@hotmail.com>
2024-12-27 09:55:21 +0530

    Create Hyponatremia.py
    An attempt at writing a program on hyponatremia treatment

---
+++ Hyponatremia.py
@@ -0,0 +1,16 @@
+#definition
+mild_symptoms = ["nauseous","having headache","fatigued"]
+moderate_symptoms = ["in confusion", "having muscle cramps", "feeling weakness"]
+severe_symptoms = ["having seizures", "in coma", "in respiratory arrest"]
+
+
+
+print("Hyponatremia Treatment")
+print("")
+duration = int(input("What is the duration of hyponatremia (in hours)? ")
+if duration > 48 hours:
+  onset = "chronic"
+else:
+  onset = "acute"
+if onset == "acute":
+  print("The patent needs to be admitted in the hospital")
